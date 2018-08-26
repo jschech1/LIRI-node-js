1600affca15e7ae2a78edbd756484bb1ed392820
Jordan Schechter <jordan@Jordans-MacBook-Pro.local>
2018-08-26 15:37:01 -0600

    files update

---
+++ liri.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+console.log("-----------");
+console.log("Stuff I need");
+console.log(keys);
+console.log("-----------");
+
+// var Spotify = require('node-spotify-api');
